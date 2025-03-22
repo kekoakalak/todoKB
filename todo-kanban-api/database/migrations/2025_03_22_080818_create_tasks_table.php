d741f18a317cf0fe2ee7075fe7a47baea7bc87d7
kekoakalak <mackenzieborneo@gmail.com>
2025-03-22 16:27:24 +0800

    Edit migration and schema for table model

--- todo-kanban-api/database/migrations/2025_03_22_080818_create_tasks_table.php
+++ todo-kanban-api/database/migrations/2025_03_22_080818_create_tasks_table.php
@@ -4,26 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateTasksTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
+        //Create Scheme for table columns
         Schema::create('tasks', function (Blueprint $table) {
+
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Links task to user
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('status')->default('todo'); // 'todo', 'in_progress', 'done'
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+
     public function down()
     {
         Schema::dropIfExists('tasks');
